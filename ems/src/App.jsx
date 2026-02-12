a6f80f25195b94222602db9a1db6673a89d9b83d
harshu-x <harshalpet07@gmail.com>
2026-02-12 20:17:03 +0530

    Login

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -11,16 +11,24 @@ const App = () => {
 
   const [user, setUser] = useState(null);
   const authData = useContext(AuthContext)
-  console.log(authData);
-  console.log(authData.employee)
+  useEffect(()=>{
+      if(authData){
+         const loggedInUser = localStorage.getItem("loggedInUser")
+        if(loggedInUser){
+          setUser(loggedInUser.role)
+        }
+        }
+  },[authData]);
+ 
   const handleLogin =(email ,password)=>{
        if(email =='admin@me.com' && password =='123'){
          setUser('admin')
-         console.log(user);
+         localStorage.setItem('loggedInUser' , JSON.stringify({role:'admin'}))
        }
-       else if(authData){
+       else if(authData && authData.employee.find((e)=> email == e.email && e.password == password)){
              setUser('employee')
-        console.log(user);
+           localStorage.setItem('loggedInUser' , JSON.stringify({role:'employee'}))
+       
        }
    
        
