0e01e6b5e137165179406ac22bae38d53ed09ecd
harshu-x <harshalpet07@gmail.com>
2026-02-12 17:46:31 +0530

    Auth Provider

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,39 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Login from './components/Auth/Login'
 import { useState } from 'react'
 import EmployeeDashboard from './components/DashBoard/EmployeeDashboard'
 import AdminDashboard from './components/DashBoard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
 const App = () => {
-  useEffect(() => {
-
-  const data = localStorage.getItem("employees");
-
-  if (!data) {
-    setLocalStorage(); // only first time
-  }
-
-}, []);
+ 
 
 
   const [user, setUser] = useState(null);
-
+  const authData = useContext(AuthContext)
+  console.log(authData);
+  console.log(authData.employee)
   const handleLogin =(email ,password)=>{
        if(email =='admin@me.com' && password =='123'){
          setUser('admin')
          console.log(user);
        }
-       else if(email=='user@me.com' && password =='123'){
-        setUser('employee')
+       else if(authData){
+             setUser('employee')
         console.log(user);
        }
+   
+       
        else{
           alert("Invalid Credentials");
        }
   }
 
 
- 
+
   return (
     <>
     {!user ? <Login  handleLogin={handleLogin}/> : ' '}
--- ems/src/context/AuthProvider.jsx
+++ ems/src/context/AuthProvider.jsx
@@ -1,49 +1,29 @@
-// import React, { createContext } from 'react'
-// import { useState } from 'react';
-// import { getLocalStorage } from '../utils/localStorage';
-// export const AuthContext = createContext()
-// const AuthProvider = ({children}) => {
+import React, { createContext } from 'react'
+import { useState,useEffect } from 'react';
+import { setLocalStorage,getLocalStorage } from '../utils/localStorage';
+export const AuthContext = createContext()
+const AuthProvider = ({children}) => {
 
-//     const [userData, setUserData] = useState(null)
-//      const data=getLocalStorage();
-//      console.log(data);
-//   return (
-//     <AuthContext.Provider value={"Harshal"}>
-//         {children}
-//     </AuthContext.Provider>
-//   )
-// }
+    const [userData, setUserData] = useState(null)
 
-// export default AuthProvider
-import React, { createContext, useEffect, useState } from 'react'
-import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
+   useEffect(() => {
 
-export const AuthContext = createContext();
+  if (!localStorage.getItem("employees")) {
+    setLocalStorage();
+  }
 
-const AuthProvider = ({ children }) => {
+  const data=getLocalStorage();
+  setUserData(data);
 
-  const [userData, setUserData] = useState(null);
+}, []);
 
-  useEffect(() => {
 
-    const employees = localStorage.getItem("employees");
-
-    if (!employees) {
-      setLocalStorage(); // initialize only once
-    }
-
-    const data = getLocalStorage();
-    console.log(data);
-
-    setUserData(data);
-
-  }, []);
-
+     
   return (
     <AuthContext.Provider value={userData}>
-      {children}
+        {children}
     </AuthContext.Provider>
   )
 }
 
-export default AuthProvider;
+export default AuthProvider
