e77fde93cf209e9441021be664e20733a4e3d7d5
harshu-x <harshalpet07@gmail.com>
2026-02-13 11:49:26 +0530

    Log Out Updated

--- ems/src/components/others/Header.jsx
+++ ems/src/components/others/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import { setLocalStorage } from '../../utils/localStorage';
 const Header = () => {
   // const [username, setUsername] = useState('');
 
@@ -9,13 +10,18 @@ const Header = () => {
   // else{
   //   setUsername(data.firstName);
   // }
+
+  const logOutUser =()=>{
+     localStorage.setItem('loggedInUser' , ' ');
+     window.location.reload();
+  }
   return (
     <div className='flex items-end justify-between'>
       <h1 className='text-2xl'>hello
          <br /> 
         <span className='text-3xl font-semibold'>hi👋</span>
       </h1>
-      <button className='bg-red-500 text-lg font-medium  text-white px-5 py-2 rounded-sm'>log out!!..</button>
+      <button onClick={logOutUser} className='bg-red-500 text-lg font-medium  text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
 }
