3e90f3ece5c1602b40f8b2f390c288129201ab1b
harshu-x <harshalpet07@gmail.com>
2026-02-13 17:59:31 +0530

    Admin Page Update

--- ems/src/components/others/AllTask.jsx
+++ ems/src/components/others/AllTask.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
+  const authData = useContext(AuthContext)
+  console.log(authData.employee);
   return (
-    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto'>
-      <div className='bg-red-400 py-2 mb-2 px-4 flex justify-between rounded'>
-        <h2>Harshal</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded'>
+       <div className='bg-red-400 py-2 mb-2 px-4 flex justify-between rounded'>
+        <h2 className='text-lg font-medium w-1/5 '>Employee Name</h2>
+        <h3 className='text-lg font-medium w-1/5 '>New Task</h3>
+        <h5 className='text-lg font-medium w-1/5 '>Active Task</h5>
+           <h5 className='text-lg font-medium w-1/5 '>Completed</h5>
+             <h5 className='text-lg font-medium w-1/5 '>Failed</h5>
+      </div>
+      <div className='overflow-auto'>
+         {authData.employee.map(function(elem){
+        return  <div className=' py-2 mb-2 px-4 flex justify-between rounded'>
+        <h2 className='text-lg font-medium w-1/5 text-blue-600'>{elem.firstName}</h2>
+        <h5 className='text-lg font-medium w-1/5 text-yellow-600'>{elem.taskCounts.newTask}</h5>
+           <h5 className='text-lg font-medium w-1/5 text-green-600'>{elem.taskCounts.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-green-600'>{elem.taskCounts.completed}</h5>
+             <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
       </div>
 
-
-      <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Harshal</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
+      })}
       </div>
+     
+      
 
-      <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Harshal</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
+     
+    
     </div>
   )
 }
